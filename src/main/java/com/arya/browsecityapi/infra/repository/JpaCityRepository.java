16aa46b86bead738d5adf0651cbe61a6e92d2b6e
H-A-L-F <64455337+AryaJayadi@users.noreply.github.com>
2025-01-13 19:56:15 +0700

    .

--- src/main/java/com/arya/browsecityapi/infra/repository/JpaCityRepository.java
+++ src/main/java/com/arya/browsecityapi/infra/repository/JpaCityRepository.java
@@ -2,14 +2,16 @@ package com.arya.browsecityapi.infra.repository;
 
 import com.arya.browsecityapi.infra.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface JpaCityRepository extends JpaRepository<CityEntity, Long> {
 
-    List<CityEntity> findAllByName(String name);
+    @Query("SELECT c FROM CityEntity c WHERE c.name LIKE :name%")
+    List<CityEntity> findAllByName(@Param("name") String name);
 
 }
