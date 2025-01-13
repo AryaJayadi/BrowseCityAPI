9457854ef2e6ea4be1a512078301dcd0eb8c6c8b
H-A-L-F <64455337+AryaJayadi@users.noreply.github.com>
2025-01-13 23:00:49 +0700

    add validated

--- src/main/java/com/arya/browsecityapi/api/CityController.java
+++ src/main/java/com/arya/browsecityapi/api/CityController.java
@@ -5,12 +5,14 @@ import com.arya.browsecityapi.api.model.SuggestCityApiResponse;
 import com.arya.browsecityapi.app.CityScoreWrapper;
 import com.arya.browsecityapi.app.service.ICityService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
+@Validated
 @RequiredArgsConstructor
 public class CityController implements CityApi {
 
