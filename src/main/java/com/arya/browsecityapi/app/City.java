a33398ba4a002ade164d77901731c3740062f1ec
H-A-L-F <64455337+AryaJayadi@users.noreply.github.com>
2025-01-13 18:25:45 +0700

    City App Object

---
+++ src/main/java/com/arya/browsecityapi/app/City.java
@@ -0,0 +1,24 @@
+package com.arya.browsecityapi.app;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class City {
+
+    private String id;
+    private String name;
+    private String ascii;
+    private List<String> altNames;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private int population;
+}
