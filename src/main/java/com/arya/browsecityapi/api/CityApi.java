e8d753b10d003ec239670e9ac4f3d9ba3883c32b
H-A-L-F <64455337+AryaJayadi@users.noreply.github.com>
2025-01-13 20:32:03 +0700

    suggestionservice

--- src/main/java/com/arya/browsecityapi/api/CityApi.java
+++ src/main/java/com/arya/browsecityapi/api/CityApi.java
@@ -17,7 +17,7 @@ public interface CityApi {
             summary = "Search for cities based on a query",
             description = "Returns city suggestions based on partial or complete name matching, optionally considering proximity to the provided latitude and longitude for better scoring",
             parameters = {
-                    @Parameter(name = "q", description = "Partial or complete search term", required = false),
+                    @Parameter(name = "q", description = "Partial or complete search term", required = true),
                     @Parameter(name = "latitude", description = "Latitude of the caller for proximity scoring", required = false),
                     @Parameter(name = "longitude", description = "Longitude of the caller for proximity scoring", required = false)
             },
@@ -28,7 +28,7 @@ public interface CityApi {
     )
     @GetMapping("/suggestions")
     List<SuggestCityApiResponse> getSuggestions(
-            @RequestParam(value = "q", required = false, defaultValue = "") String q,
+            @RequestParam(value = "q", required = true) String q,
             @RequestParam(value = "latitude", required = false) BigDecimal latitude,
             @RequestParam(value = "longitude", required = false) BigDecimal longitude
     );
--- src/main/java/com/arya/browsecityapi/app/service/CityService.java
+++ src/main/java/com/arya/browsecityapi/app/service/CityService.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,8 +23,52 @@ public class CityService implements ICityService {
 
     @Override
     public List<CityScoreWrapper> getCitySuggestions(String q, BigDecimal latitude, BigDecimal longitude) {
-        List<CityScoreWrapper> res = new ArrayList<>();
         List<City> cities = repository.findAllByName(q);
-        return res;
+
+        if (q.isBlank()) {
+            throw new InvalidParameterException("'q' parameter cannot be null");
+        } else {
+            return cities.stream().map(o -> CityScoreWrapper.builder()
+                    .city(o)
+                    .score(calculateCitySuggestionScore(o.getName(), o.getLatitude(), o.getLongitude(), q, latitude, longitude, o.getPopulation()))
+                    .build()
+            ).sorted(Comparator.comparing(CityScoreWrapper::getScore).reversed()).collect(Collectors.toList());
+        }
+    }
+
+    private Double calculateNameSimilarity(String name1, String name2) {
+        name1 = name1.toLowerCase();
+        name2 = name2.toLowerCase();
+        return (double) (name1.length() - name1.replace(name2, "").length()) / (double) name1.length();
+    }
+
+    private Double calculateDistance(double cityLat, double cityLong, double queryLat, double queryLong) {
+        double latDistance = Math.toRadians(cityLat - queryLat);
+        double lonDistance = Math.toRadians(cityLong - queryLong);
+        double a = Math.sin(latDistance / 2.0) * Math.sin(latDistance / 2.0) + Math.cos(Math.toRadians(queryLat)) * Math.cos(Math.toRadians(cityLat)) * Math.sin(lonDistance / 2.0) * Math.sin(lonDistance / 2.0);
+        double c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1.0 - a));
+        return 6371.0 * c;
+    }
+
+    private Double calculatePopulationFactor(long population) {
+        long maxPopulation = 10000000L;
+        return Math.log((double) (population + 1L)) / Math.log((double) (maxPopulation + 1L));
+    }
+
+    private Double calculateCitySuggestionScore(String cityName, BigDecimal cityLat, BigDecimal cityLong, String query, BigDecimal queryLat, BigDecimal queryLong, long population) {
+        double TEXT_WEIGHT = 0.5;
+        double DISTANCE_WEIGHT = 0.3;
+        double POPULATION_WEIGHT = 0.2;
+        double textSimilarity = this.calculateNameSimilarity(cityName, query);
+        double populationFactor = this.calculatePopulationFactor(population);
+        double score;
+        if (queryLat != null && queryLong != null) {
+            score = this.calculateDistance(cityLat.doubleValue(), cityLong.doubleValue(), queryLat.doubleValue(), queryLong.doubleValue());
+            double finalScore = TEXT_WEIGHT * textSimilarity + DISTANCE_WEIGHT * (1.0 - score / 10000.0) + POPULATION_WEIGHT * populationFactor;
+            return (Math.round(finalScore * 10.0) / 10.0);
+        } else {
+            score = 0.8999999999999999 * textSimilarity + POPULATION_WEIGHT * populationFactor;
+            return (Math.round(score * 10.0) / 10.0);
+        }
     }
 }
